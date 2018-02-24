724f5c1cd8024f91692966c14086d3f2c7a165cf
Vipul Kanade <vipultechjoy@gmail.com>
2018-02-23 19:21:44 -0800

    Added Proof of Work

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const SHA256 = require('crypto-js/sha256');
   // timestamp    -> when it was created
   // data         -> any type of data (e.g. details of transaction)
   // previousHash -> string containing previous Hash of the block before (ensures integrity of Blockchain)
+  // nonce        -> Random number
 **/
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
@@ -15,6 +16,7 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     // Calculate the hash function of the block.
@@ -22,13 +24,23 @@ class Block {
     // this will identify our block on blockchain,
     // using SHA256 for this, javascript does not support it by default will require 'crypto-js' library for this.
     calculateHash() {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock() {
@@ -41,7 +53,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -64,13 +76,17 @@ class Blockchain {
 }
 
 let vipCoin = new Blockchain();
+
+console.log("Mining Block 1....");
 vipCoin.addBlock(new Block(1, "02/20/2018", {amount: 4}));
+
+console.log("Mining Block 2....");
 vipCoin.addBlock(new Block(2, "02/22/2018", {amount: 10}));
 
-console.log('Is blockchain valid? ' + vipCoin.isChainValid());
-
-vipCoin.chain[1].data = { amount : 100 };
-vipCoin.chain[1].hash = vipCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + vipCoin.isChainValid());
+// console.log('Is blockchain valid? ' + vipCoin.isChainValid());
+//
+// vipCoin.chain[1].data = { amount : 100 };
+// vipCoin.chain[1].hash = vipCoin.chain[1].calculateHash();
+//
+// console.log('Is blockchain valid? ' + vipCoin.isChainValid());
 //console.log(JSON.stringify(vipCoin, null, 4));
